88a55033501b51eb8f7e9b49183c4498db6ae785
Peter Zadrozny <246755631+yv01p@users.noreply.github.com>
2026-05-28 18:12:26 +0000

    refactor(subcontracting): extract validation cluster to subcontracting/validation.py

--- erpnext/controllers/subcontracting_controller.py
+++ erpnext/controllers/subcontracting_controller.py
@@ -11,6 +11,7 @@ from frappe.model.mapper import get_mapped_doc
 from frappe.utils import cint, flt, get_link_to_form
 
 from erpnext.controllers.stock_controller import StockController
+from erpnext.controllers.subcontracting import validation
 from erpnext.stock.doctype.batch.batch import get_batch_qty
 from erpnext.stock.doctype.serial_and_batch_bundle.serial_and_batch_bundle import (
 	combine_datetime,
@@ -109,127 +110,16 @@ class SubcontractingController(StockController):
 			self.calculate_items_qty_and_amount()
 
 	def validate_rejected_warehouse(self):
-		for item in self.get("items"):
-			if flt(item.rejected_qty) and not item.rejected_warehouse:
-				if self.rejected_warehouse:
-					item.rejected_warehouse = self.rejected_warehouse
-				else:
-					frappe.throw(
-						_("Row #{0}: Rejected Warehouse is mandatory for the rejected Item {1}").format(
-							item.idx, item.item_code
-						)
-					)
-
-			if item.get("rejected_warehouse") and (item.get("rejected_warehouse") == item.get("warehouse")):
-				frappe.throw(
-					_("Row #{0}: Accepted Warehouse and Rejected Warehouse cannot be same").format(item.idx)
-				)
+		validation.validate_rejected_warehouse(self)
 
 	def remove_empty_rows(self):
-		for key in ["service_items", "items", "supplied_items", "received_items"]:
-			if self.get(key):
-				idx = 1
-				for item in self.get(key)[:]:
-					if not (item.get("item_code") or item.get("main_item_code")):
-						self.get(key).remove(item)
-					else:
-						item.idx = idx
-						idx += 1
+		validation.remove_empty_rows(self)
 
 	def set_items_conversion_factor(self):
-		for item in self.get("items"):
-			if not item.conversion_factor:
-				item.conversion_factor = 1
+		validation.set_items_conversion_factor(self)
 
 	def validate_items(self):
-		for item in self.items:
-			is_stock_item, is_sub_contracted_item = frappe.get_value(
-				"Item", item.item_code, ["is_stock_item", "is_sub_contracted_item"]
-			)
-
-			if not is_stock_item:
-				frappe.throw(_("Row {0}: Item {1} must be a stock item.").format(item.idx, item.item_name))
-
-			if (
-				self.doctype == "Subcontracting Inward Order"
-				and item.delivery_warehouse == self.customer_warehouse
-			):
-				frappe.throw(
-					_(
-						"Row {0}: Delivery Warehouse cannot be same as Customer Warehouse for Item {1}."
-					).format(item.idx, get_link_to_form("Item", item.item_code))
-				)
-
-			if not item.get("type") and not item.get("is_legacy_scrap_item"):
-				if not is_sub_contracted_item:
-					frappe.throw(
-						_("Row {0}: Item {1} must be a subcontracted item.").format(item.idx, item.item_name)
-					)
-
-				if self.doctype != "Subcontracting Receipt":
-					order_item_doctype = (
-						"Purchase Order Item"
-						if self.doctype == "Subcontracting Order"
-						else "Sales Order Item"
-					)
-
-					order_name = (
-						self.purchase_order if self.doctype == "Subcontracting Order" else self.sales_order
-					)
-					order_item_field = frappe.scrub(order_item_doctype)
-
-					if not item.get(order_item_field):
-						frappe.throw(
-							_("Row {0}: Item {1} must be linked to a {2}.").format(
-								item.idx, item.item_name, order_item_doctype
-							)
-						)
-
-					pending_qty = flt(
-						flt(
-							get_pending_subcontracted_quantity(
-								order_item_doctype,
-								order_name,
-							).get(item.get(order_item_field))
-						)
-						/ item.subcontracting_conversion_factor,
-						frappe.get_precision(
-							order_item_doctype,
-							"qty",
-						),
-					)
-
-					if item.qty > pending_qty:
-						frappe.throw(
-							_(
-								"Row {0}: Item {1}'s quantity cannot be higher than the available quantity."
-							).format(item.idx, item.item_name)
-						)
-
-				if self.doctype not in ["Subcontracting Inward Order", "Subcontracting Receipt"]:
-					item.amount = item.qty * item.rate
-
-				if item.bom:
-					is_active, bom_item = frappe.get_value("BOM", item.bom, ["is_active", "item"])
-
-					if not is_active:
-						frappe.throw(
-							_("Row {0}: Please select an active BOM for Item {1}.").format(
-								item.idx, item.item_name
-							)
-						)
-					if bom_item != item.item_code:
-						frappe.throw(
-							_("Row {0}: Please select an valid BOM for Item {1}.").format(
-								item.idx, item.item_name
-							)
-						)
-				else:
-					frappe.throw(
-						_("Row {0}: Please select a BOM for Item {1}.").format(item.idx, item.item_name)
-					)
-			else:
-				item.bom = None
+		validation.validate_items(self)
 
 	def __get_data_before_save(self):
 		item_dict = {}
